0a6e8b345f299bc09b1877e0281a914e0dbfa950
mattroz <matveyrozanov@gmail.com>
2017-07-15 22:05:34 +0300

    Implemented DFS (it was beer coding so I need to refactor tis part of code AQAIC)

--- env_gen.py
+++ env_gen.py
@@ -1,15 +1,17 @@
 import pygame
 import sys
 import random
+import os
+from time import sleep
 
 # main graphical parameters
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 640
-CELL_SIZE = 10
+CELL_SIZE = 20
 CELL_AMOUNT_X = WINDOW_WIDTH // CELL_SIZE
 CELL_AMOUNT_Y = WINDOW_HEIGHT // CELL_SIZE
-START_CELL = (0, 0)
-GOAL_CELL = (CELL_AMOUNT_X-1, CELL_AMOUNT_Y-1)
+START_CELL = (1, 1)
+GOAL_CELL = (CELL_AMOUNT_X - 2, CELL_AMOUNT_Y - 2)
 
 assert ((WINDOW_WIDTH % CELL_SIZE == 0) and (WINDOW_HEIGHT % CELL_SIZE == 0)), "Change cell size"
 
@@ -18,6 +20,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+TURQUOISE = (102, 255, 255)
 
 
 # fill cells with colors
@@ -29,7 +32,7 @@ def draw_grid(field, window_w, window_h, cell_size):
                 color_cell(current_cell, WHITE)
             elif field[current_cell] == 1:
                 color_cell(current_cell, BLUE)
-            elif field[current_cell] == 2:
+            elif field[current_cell] == 2 or field[current_cell] == 3:
                 color_cell(current_cell, RED)
 
 
@@ -45,16 +48,22 @@ def create_blank_grid():
 # TODO: should call constants either as arguments or as global variables!
 def fill_with_obstacles(grid, rarefaction):
     obstacle_num = WINDOW_WIDTH
+    # generate obstacles according to rarefaction seed
     for cell in grid:
-        if obstacle_num == 0:
+        # make some kind of border for the field
+        if (cell[0] == 0) or (cell[0] == CELL_AMOUNT_X - 1) \
+                or (cell[1] == 0) or (cell[1] == CELL_AMOUNT_Y - 1):
+            grid[cell] = 1
+        elif obstacle_num == 0:
             return grid
         else:
             # rollin' the dice
             dice = random.randint(0, 100000)
             grid[cell] = (1 if dice > rarefaction else 0)
             obstacle_num -= (1 if grid[cell] == 1 else 0)
+
     grid[START_CELL] = 2
-    grid[GOAL_CELL] = 2
+    grid[GOAL_CELL] = 3
     return grid
 
 
@@ -64,11 +73,41 @@ def color_cell(cell, color):
     pygame.draw.rect(SCREENSURF, color, (_x, _y, CELL_SIZE, CELL_SIZE))
 
 
+def breadth_first_search(field, visits_grid, current_cell, goal_cell):
+    sleep(0.1)
+    pygame.display.update()
+    if field[current_cell[0], current_cell[1]] == field[goal_cell[0], goal_cell[1]]:
+        print("Goal've been achieved")
+        exit()
+    # down, right, up, left
+    x_direction = [0, 1, 0, -1]
+    y_direction = [1, 0, -1, 0]
+    directions = len(x_direction)
+
+    neighbour = []
+    for i in range(2):
+        neighbour.append(0)
+    visits_grid[current_cell[0], current_cell[1]] = 1
+    color_cell(current_cell, TURQUOISE)
+    for step in range(directions):
+        neighbour[0] = current_cell[0] + x_direction[step]
+        neighbour[1] = current_cell[1] + y_direction[step]
+        if (field[(neighbour[0], neighbour[1])] == 0 or field[(neighbour[0], neighbour[1])] == 3) \
+                and visits_grid[(neighbour[0], neighbour[1])] != 1:
+            breadth_first_search(field, visits_grid, neighbour, goal_cell)
+
+
 def main():
     pygame.init()
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 10)
     global SCREENSURF
     SCREENSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     SCREENSURF.fill(WHITE)
+
+    visits = {}
+    for x in range(CELL_AMOUNT_X):
+        for y in range(CELL_AMOUNT_Y):
+            visits[(x, y)] = 0
 
     field = create_blank_grid()
     fill_with_obstacles(field, 85000)
@@ -80,7 +119,7 @@ def main():
                 sys.exit()
         pygame.display.update()
         draw_grid(field, WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
-
+        breadth_first_search(field, visits, START_CELL, GOAL_CELL)
 
 if __name__ == '__main__':
     main()
